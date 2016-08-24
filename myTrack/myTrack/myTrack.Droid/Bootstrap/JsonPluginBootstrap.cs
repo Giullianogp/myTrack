12f148cd7092ee4ec31215d943f92f5096d76094
Giullianogp <giullianogp@hotmail.com>
2016-08-24 09:01:39 -0300

    Initial Commit :metal:

---
+++ myTrack/myTrack/myTrack.Droid/Bootstrap/JsonPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace myTrack.Droid
+{
+    public class JsonPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.Json.PluginLoader>
+    {
+    }
+}
