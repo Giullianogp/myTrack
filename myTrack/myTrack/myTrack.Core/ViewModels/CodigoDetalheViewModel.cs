8d1b340810cc7c8c90aab7336971ceb0bab8958a
Giullianogp <giullianogp@hotmail.com>
2016-08-29 14:28:23 -0300

    Width ListView

---
+++ myTrack/myTrack/myTrack.Core/ViewModels/CodigoDetalheViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Core.ViewModels;
+
+namespace myTrack.Core.ViewModels
+{
+    public class CodigoDetalheViewModel : MvxViewModel
+    {
+        public CodigoDetalheViewModel()
+        {
+
+        }
+    }
+}
