19178722b9cbb69f669b22b492f8bc61cf2f7dee
Giullianogp <giullianogp@hotmail.com>
2016-08-30 07:46:13 -0300

    Adicionado WebService

--- myTrack/myTrack/myTrack.Core/ViewModels/CodigoDetalheViewModel.cs
+++ myTrack/myTrack/myTrack.Core/ViewModels/CodigoDetalheViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using MvvmCross.Core.ViewModels;
@@ -11,7 +13,31 @@ namespace myTrack.Core.ViewModels
     {
         public CodigoDetalheViewModel()
         {
+            //var url = "http://developers.agenciaideias.com.br/correios/rastreamento/json/{0}";
 
+            //var client = new HttpClient();
+            //client.DefaultRequestHeaders.AcceptLanguage.Clear();
+            //client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("pt-br", 0.5));
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //var resp = client.GetAsync(string.Format(url, IdCodigo.Text)).Result;
+
+            //List<Rastreamento> retorno;
+
+            //try
+            //{
+            //    retorno = resp.IsSuccessStatusCode ? JsonConvert.DeserializeObject<List<Rastreamento>>(resp.Content.ReadAsStringAsync().Result) : null;
+            //}
+            //catch (Exception ex)
+            //{
+            //    retorno = null;
+            //}
+
+            //if (retorno != null)
+            //    listview.ItemsSource = retorno.OrderByDescending(x => x.DataD);
+            //else
+            //{
+
+            //}
         }
     }
 }
